6aa9046d59b8a901620c2e33dbba672f0fb95fab
axellelecroq <lecroq.axelle@gmail.com>
2022-01-16 19:50:33 +0100

    search works with one keyword

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,10 @@
 from flask import render_template, request
+from nested_lookup import nested_lookup
 
 from app.app import app, statics
 from .utils.generic import get_json
+
+
 
 
 @app.route("/")
@@ -34,9 +37,38 @@ def search():
     data = get_json(statics + "/records.json")
     search_list = [i for i in request.args.values()]
     results = []
+    contributors = set(nested_lookup("contributor", data))
+
 
     if "on" in search_list:
         results = data
 
+    if search_list[0]:
+        for i in data:
+            if search_list[0] in i["title"]:
+                results.append(i)
+    
+    elif search_list[1]:
+        for i in data:
+            try:
+                if search_list[1] in i["coverage"]:
+                    results.append(i)
+            except: continue
+    
+    elif search_list[2]:
+        for i in data:
+            try:
+                if search_list[2] in i["date"]:
+                    results.append(i)
+            except: continue
+
+    elif search_list[3]:
+        for i in data:
+            try:
+                if search_list[3] in i["contributor"]:
+                    results.append(i)
+            except: continue
+
+
     count_letters= len(results)
-    return render_template("pages/search.html", results=results, count=count_letters)
+    return render_template("pages/search.html", results=results, count=count_letters, institutions=contributors)
